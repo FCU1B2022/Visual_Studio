11e49b9df4bb6a84f43f8df68c2c92aa66c921aa
eason280711 <eason280711@gmail.com>
2023-03-01 22:46:19 +0800

    Update

--- source/main.c
+++ source/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include "search.h"
+//#include "search.h"
 
 int main()
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
 
     int arr[] = { 2, 3, 4, 10, 40 };
 
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 10;
 
+    /*
     int result = binary_search(arr, 0, n - 1, x);
     (result == -1) ? printf("Element is not present in array")
                    : printf("Element is present at index %d", result);
+    */
     return 0;
 }
